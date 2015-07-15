88803c0ddead4614a82be1c72354d3b38f07a607
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-15 08:53:20 +0200

    extracted method from Calculator class to main

--- the_TDD_course/essential-tdd-assertion/calculator.rb
+++ the_TDD_course/essential-tdd-assertion/calculator.rb
@@ -6,23 +6,18 @@ class Calculator
   def subtract(x, y)
     x - y
   end
+end
 
-
-  def verify(expected, actual, message)
-    if actual == expected
-      puts "\033[32m #{message} Passed \033[0m"
-    else
-      puts "\e[31m #{message} Failed \e[0m"
-    end
+def verify(expected, actual, message)
+  if actual == expected
+    puts "\033[32m #{message} Passed \033[0m \n"
+  else
+    puts "Expected : #{expected} but got Actual : #{actual} \n"
+    puts "\e[31m #{message} Failed \e[0m \n"
   end
 end
 
 calc = Calculator.new
-calc.verify(2, calc.sum(1,1), 'Sum')
-
-result =  calc.subtract(2,1)
-if result == 1
-  puts "\033[32m Subtraction Passed \033[0m"
-else
-  puts "\e[31m Subtraction Failed \e[0m"
-end
+verify(2, calc.sum(1, 1), 'Sum')
+verify(1, calc.subtract(2, 1), 'Subtraction')
+verify(3, calc.sum(1, 1), 'Sum')
