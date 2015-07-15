a6b0c95029e91d3846bc5b0808682eeb22c0b866
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-15 08:43:30 +0200

    Added subtraction operation

--- the_TDD_course/essential-tdd-assertion/calculator.rb
+++ the_TDD_course/essential-tdd-assertion/calculator.rb
@@ -2,6 +2,10 @@ class Calculator
   def sum(x, y)
     x + y
   end
+
+  def subtract(x, y)
+    x - y
+  end
 end
 
 calc = Calculator.new
@@ -11,4 +15,11 @@ if result == 2
   puts "\033[32m Sum Passed \033[0m"
 else
   puts "\e[31m Sum Failed \e[0m"
+end
+
+result =  calc.subtract(2,1)
+if result == 1
+  puts "\033[32m Subtraction Passed \033[0m"
+else
+  puts "\e[31m Subtraction Failed \e[0m"
 end
