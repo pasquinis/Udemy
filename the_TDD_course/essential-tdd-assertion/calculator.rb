823fdf9a96ca94108a71523b7edc7a8d8ad48b4e
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-15 08:59:07 +0200

    Added divide operation and check the result

--- the_TDD_course/essential-tdd-assertion/calculator.rb
+++ the_TDD_course/essential-tdd-assertion/calculator.rb
@@ -10,6 +10,10 @@ class Calculator
   def multiply(x, y)
     x * y
   end
+
+  def divide(x, y)
+    x / y
+  end
 end
 
 def verify(expected, actual, message)
@@ -26,3 +30,4 @@ verify(2, calc.sum(1, 1), 'Sum')
 verify(1, calc.subtract(2, 1), 'Subtraction')
 verify(3, calc.sum(1, 1), 'Sum')
 verify(6, calc.multiply(3, 2), 'Multiply')
+verify(5, calc.divide(10, 2), 'Divide')
