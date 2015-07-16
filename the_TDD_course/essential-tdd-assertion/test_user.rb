9addfe2f1bbf178b2fa6336a8c18f5363b71b708
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-16 08:57:44 +0200

    test_user_is_not_in_admin_role shows the hardcode implementation

--- the_TDD_course/essential-tdd-assertion/test_user.rb
+++ the_TDD_course/essential-tdd-assertion/test_user.rb
@@ -12,11 +12,19 @@ end
 
 class TestUser < Minitest::Test
 
-  def test_a_user_can_assign_a_role
+  def test_user_is_in_admin_role
     user = User.new
 
     user.assign_role('admin')
 
     assert user.in_role?('admin'), 'user is not in admin role'
+  end
+
+  def test_user_is_not_in_admin_role
+    user = User.new
+
+    user.assign_role('admin')
+
+    assert !user.in_role?('admin'), 'user is in admin role'
   end
 end
