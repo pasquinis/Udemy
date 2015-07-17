a12e63f4ea399a80c53ba17231ff19c942678059
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-17 06:51:00 +0000

    Added tmux for have a desktop

---
+++ the_TDD_course/tmux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+tmux start-server
+tmux new-session -d -s Udemy -n code
+tmux new-window -tUdemy:0 -n code
+tmux split-window -tUdemy:0 -p 30 -h
+tmux new-window -tUdemy:1 -n ssh
+
+tmux send-keys -tUdemy:0 'cd ~/projects/Udemy; clear' C-m
+tmux send-keys -tUdemy:1 'cd ~/project/Udemy; clear' C-m
+
+tmux select-window -tUdemy:0
+tmux attach-session -d -tUdemy
