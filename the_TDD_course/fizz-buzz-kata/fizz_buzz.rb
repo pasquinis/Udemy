672f02b570afff1b7b2e08337870b9f241ad07c0
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-20 11:25:58 +0000

    Added test for multiple of 3 and 5

--- the_TDD_course/fizz-buzz-kata/fizz_buzz.rb
+++ the_TDD_course/fizz-buzz-kata/fizz_buzz.rb
@@ -6,4 +6,16 @@ class FizzBuzz
     def numbers
         (1..@max_value).to_a
     end
+
+    def generate
+        numbers.collect do |number|
+            if (number % 3 == 0)
+                'Fizz'
+            elsif (number % 5 == 0)
+                'Buzz'
+            else
+                number
+            end
+        end
+    end
 end
--- the_TDD_course/fizz-buzz-kata/test_fizz_buzz.rb
+++ the_TDD_course/fizz-buzz-kata/test_fizz_buzz.rb
@@ -9,4 +9,20 @@ class TestFizzBuzz < Minitest::Test
 
         assert_equal((1..100).to_a, result)
     end
+
+    def test_generate_fizz_for_multiple_of_3
+        obj = FizzBuzz.new(3)
+
+        result = obj.generate
+
+        assert_equal('Fizz', result[2])
+    end
+
+    def test_generate_buzz_for_multiple_of_5
+        obj = FizzBuzz.new(5)
+
+        result = obj.generate
+
+        assert_equal('Buzz', result[4])
+    end
 end
