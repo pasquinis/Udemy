6f409dc5a0ab8c9f3be0e5b14789b9292dd09276
Simone Pasquini <simone.pasquini@neomobile.com>
2015-07-24 07:20:26 +0000

    Modified tmux export

--- the_TDD_course/tmux.sh
+++ the_TDD_course/tmux.sh
@@ -3,11 +3,11 @@
 tmux start-server
 tmux new-session -d -s Udemy -n code
 tmux new-window -tUdemy:0 -n code
-tmux split-window -tUdemy:0 -p 30 -h
+tmux split-window -tUdemy:0 -p 40 -h
 tmux new-window -tUdemy:1 -n ssh
 
 tmux send-keys -tUdemy:0 'cd ~/projects/Udemy; clear' C-m
-tmux send-keys -tUdemy:1 'cd ~/project/Udemy; clear' C-m
+tmux send-keys -tUdemy:1 'cd ~/project/Udemy; export PS1="# "; clear' C-m
 
 tmux select-window -tUdemy:0
 tmux attach-session -d -tUdemy
