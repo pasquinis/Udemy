07bd7c09ffd6555dcfe1f67bb4076cb7d2325c32
Simone Pasquini <simone.pasquini@gmail.com>
2016-08-17 10:21:28 +0200

    Sometimes IPAddress is sometimes empty
    See the issue https://github.com/docker/docker/issues/21658
    I remove the smoke test

--- using-docker/chapter-8/identijenk/execute-shell.sh
+++ using-docker/chapter-8/identijenk/execute-shell.sh
@@ -18,17 +18,6 @@ sudo docker-compose $COMPOSE_ARGS up -d
 sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
 ERR=$?
 
-#Execute as last a smoke test
-if [ $ERR -eq 0 ]; then
-    IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} \
-        jenkins_identidock_1)
-    CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-    if [ $CODE -ne 200 ]; then
-        echo "Site returned " $CODE
-        ERR=1
-    fi
-fi
-
 #Stop and remove latest containers
 sudo docker-compose $COMPOSE_ARGS stop
 sudo docker-compose $COMPOSE_ARGS rm --force -v
