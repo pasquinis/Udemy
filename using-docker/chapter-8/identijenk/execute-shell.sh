13d9257dc36beb7db4e716471e4d804f4196ba04
Simone Pasquini <simone.pasquini@gmail.com>
2016-08-17 09:18:53 +0200

    Add to identidock the jenkins composer file
    Necessary for Jenkins test suite.
    Added also comments into execute-shell.sh script

--- using-docker/chapter-8/identijenk/execute-shell.sh
+++ using-docker/chapter-8/identijenk/execute-shell.sh
@@ -1,16 +1,24 @@
 #Default
 COMPOSE_ARGS="-f jenkins.yml -p jenkins"
 
-cd "using-docker/chapter-8/identijenk"
+#Move to directory with Docker file
+cd "using-docker/chapter-8/identidock"
+
+#Stop and remove latest containers
 sudo docker-compose $COMPOSE_ARGS stop
 sudo docker-compose $COMPOSE_ARGS rm --force -v
 
+#Start to build the image
 sudo docker-compose $COMPOSE_ARGS build --no-cache
+
+#Up all the containers
 sudo docker-compose $COMPOSE_ARGS up -d
 
+#Execute the unit tests
 sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
 ERR=$?
 
+#Execute as last a smoke test
 if [ $ERR -eq 0 ]; then
     IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} \
         jenkins_identidock_1)
@@ -21,6 +29,7 @@ if [ $ERR -eq 0 ]; then
     fi
 fi
 
+#Stop and remove latest containers
 sudo docker-compose $COMPOSE_ARGS stop
 sudo docker-compose $COMPOSE_ARGS rm --force -v
 
