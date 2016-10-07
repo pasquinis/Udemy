e9b60b5e8c5ffdc80386733da0be686aabfe6e68
Simone Pasquini <simone.pasquini@gmail.com>
2016-10-07 15:47:22 +0200

    Try slice to manage array

--- the-rust-programming-language/chapter-4/array-test.rs
+++ the-rust-programming-language/chapter-4/array-test.rs
@@ -2,4 +2,14 @@ fn main() {
     let a = [0; 20];
 
     println!("a elements {}, first element {}", a.len(), a[0]);
+
+
+    let a = [0, 1, 2, 3, 4];
+    println!("a elements {}, first element {}", a.len(), a[0]);
+
+    let complete_slice = &a[..];
+    println!("complete_slice elements {}, first element {}", complete_slice.len(), complete_slice[0]);
+
+    let middle_slice = &a[0..2];
+    println!("middle_slice elements {}, first element {}", middle_slice.len(), middle_slice[0]);
 }
