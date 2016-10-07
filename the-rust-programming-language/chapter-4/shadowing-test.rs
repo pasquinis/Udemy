30c05d7bff1997908c1582f5147358805a8be2a6
Simone Pasquini <simone.pasquini@gmail.com>
2016-10-07 08:49:58 +0200

    Shadowing the X variable with `let x = 42`

---
+++ the-rust-programming-language/chapter-4/shadowing-test.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x: i32 = 8;
+    {
+        println!("{}", x); // Prints "8"
+        let x = 12;
+        println!("{}", x); // Prints "12"
+    }
+    println!("{}", x); // Prints "8"
+    let x = 42;
+    println!("{}", x); // Prints "42"
+}
